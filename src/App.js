f867a7befe907df806f5c38346cab13781468db0
danielbednarz2 <danielbednarz2@gmail.com>
2021-03-22 10:49:47 -0700

    Updating project name and public files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import React from 'react'
-import { Grid } from '@material-ui/core'
-import Details from './components/Details/Details'
+import { Typography } from '@material-ui/core'
 
 const App = () => {
     return (
         <div>
-            <Grid container spacing={0} alignItems="center" justify="center" style={{height: '100vh'}}>
-                <Grid item xs={12} sm={4}>
-                    <Details />
-                </Grid>
-                <Grid item xs={12} sm={4}>
-                    <Details />
-                </Grid>
-                <Grid item xs={12} sm={4}>
-                    <Details />
-                </Grid>
-            </Grid>
+            <Typography variant='h1'>Hello, World!</Typography>
         </div>
     )
 }
